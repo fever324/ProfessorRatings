b1dd12fc3b0c09b084f146525e57759b97f43927
angelang <fangting1031@hotmail.com>
2016-09-20 15:00:43 -0400

    basic apis

---
+++ routes/courses.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Course = require('../models/Course.js');
+/* GET /courses listing. */
+router.get('/', function(req, res, next) {
+  Course.find(function (err, courses) {
+    if (err) return next(err);
+    res.json(courses);
+  });
+});
+
+/* POST /courses */
+router.post('/', function(req, res, next) {
+  Course.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /courses/id */
+router.get('/:id', function(req, res, next) {
+  Course.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* PUT /courses/:id */
+router.put('/:id', function(req, res, next) {
+  Course.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /courses/:id */
+router.delete('/:id', function(req, res, next) {
+  Course.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET users listing. */
+/*router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});*/
+
+module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Review = require('../models/Review.js');
+/* GET /reviews listing. */
+router.get('/', function(req, res, next) {
+  Review.find(function (err, reviews) {
+    if (err) return next(err);
+    res.json(reviews);
+  });
+});
+
+/* POST /reviews */
+router.post('/', function(req, res, next) {
+  Review.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /reviews/id */
+router.get('/:id', function(req, res, next) {
+  Review.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* PUT /reviews/:id */
+router.put('/:id', function(req, res, next) {
+  Review.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /reviews/:id */
+router.delete('/:id', function(req, res, next) {
+  Review.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET users listing. */
+/*router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});*/
+
+module.exports = router;
