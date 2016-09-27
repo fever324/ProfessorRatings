c5844714e3bfec47dd8e8f5fb6ccef1aa73dec72
angelang <fangting1031@hotmail.com>
2016-09-27 17:40:40 -0400

    add unit test

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+<<<<<<< 06b490d43fc047b4940f931b7585fc75b5f61929
 var express = require('express');
 var router = express.Router();
 
@@ -44,3 +45,51 @@ router.delete('/:id', function(req, res, next) {
 });
 
 module.exports = router;
+=======
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var User = require('../models/User.js');
+/* GET /users listing. */
+router.get('/', function(req, res, next) {
+  User.find(function (err, users) {
+    if (err) return next(err);
+    res.json(users);
+  });
+});
+
+/* POST /users */
+router.post('/', function(req, res, next) {
+  User.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /users/id */
+router.get('/:id', function(req, res, next) {
+  User.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* PUT /users/:id */
+router.put('/:id', function(req, res, next) {
+  User.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /users/:id */
+router.delete('/:id', function(req, res, next) {
+  User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
+>>>>>>> add unit test
