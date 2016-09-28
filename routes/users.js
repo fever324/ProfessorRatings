37799fe00bc7e22b6c4bbf65f1e426d2af2edd3a
Hongfei Li <fever324@gmail.com>
2016-09-28 11:48:37 -0400

    Register (#4)
    * Return json object during registration
    
    * Add comment

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,58 @@
-var express = require('express');
-var router = express.Router();
+  var express = require('express');
+  var router = express.Router();
 
-var mongoose = require('mongoose');
-var User = require('../models/User.js');
-/* GET /users listing. */
-router.get('/', function(req, res, next) {
-  User.find(function (err, users) {
-    if (err) return next(err);
-    res.json(users);
+  var mongoose = require('mongoose');
+  var User = require('../models/User.js');
+  /* GET /users listing. */
+  router.get('/', function(req, res, next) {
+    User.find(function (err, users) {
+      if (err) return next(err);
+      res.json(users);
+    });
   });
-});
 
-/* POST /users */
-router.post('/', function(req, res, next) {
-  User.create(req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
+  /* POST /users */
+  router.post('/', function(req, res, next) {
+    User.create(req.body, function (err, post) {
+      if (err) {
+        var message = err.code === 11000 // duplicate email address
+          ? "Email already exist"
+          : "Something wierd happened";
+        res.json({
+          success: false,
+          message: message
+        })
+        return
+      };
+      res.json({
+        success: true,
+        message: "Registration Successful",
+        user_id: post._id});
+    });
   });
-});
 
-/* GET /users/id */
-router.get('/:id', function(req, res, next) {
-  User.findById(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
+  /* GET /users/id */
+  router.get('/:id', function(req, res, next) {
+    User.findById(req.params.id, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
   });
-});
 
-/* PUT /users/:id */
-router.put('/:id', function(req, res, next) {
-  User.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
+  /* PUT /users/:id */
+  router.put('/:id', function(req, res, next) {
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
   });
-});
 
-/* DELETE /users/:id */
-router.delete('/:id', function(req, res, next) {
-  User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
+  /* DELETE /users/:id */
+  router.delete('/:id', function(req, res, next) {
+    User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
   });
-});
 
-module.exports = router;
-
+  module.exports = router;
