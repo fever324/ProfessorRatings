60568654521c4df2e7f58f599d644b3d471f38b0
Angela Wu <angelang@Angelas-MacBook-Pro.local>
2016-10-24 01:02:30 -0400

    calculate reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,29 +4,43 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Review = require('../models/Review.js');
 var Course = require('../models/Course.js');
+var Professor = require('../models/Professor.js');
 
 /* GET /reviews?course_id=x. */
 router.get('/', function(req, res, next) {
   if (req.query.course_id) {
     Course.findOne({number: req.query.course_id }, function(err, course){
       Review.find({course_id: course._id }, function(err, revs){
-        console.log(revs);
+        //console.log(revs);
         res.json(revs);
       });
     });
     return;
   }
-  res.send("404", "No such page")
-/*  Review.find(function (err, reviews) {
+  //res.send("404", "No such page")
+  Review.find(function (err, reviews) {
     if (err) return next(err);
     res.json(reviews);
-  });*/
+  });
 });
 
 /* POST /reviews */
 router.post('/', function(req, res, next) {
   Review.create(req.body, function (err, post) {
     if (err) return next(err);
+    //caculate prof's reviews
+    Professor.findOne({_id: post.prof_id }, function(err, prof){
+      var cnt = prof.number_of_reviews + 1;
+      var avg = (prof.number_of_reviews * prof.average_review + post.rating) / cnt;
+      console.log(cnt);
+      console.log(avg);
+      Professor.update({_id: post.prof_id}, {
+        number_of_reviews : cnt,
+        average_review : avg
+      }, function(err, resp) {
+        console.log(resp);
+      });
+    });
     res.json(post);
   });
 });
